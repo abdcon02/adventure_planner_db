6c3cff6b9484857a3684c6a61b8ea11f692e8201
James Nielson <james@changemachine.org>
2015-04-02 11:00:11 -0700

    Login Form and Customer login methods (login, checkLoginDB)

--- src/Customer.php
+++ src/Customer.php
@@ -46,19 +46,67 @@
 
 // This function needs to be run before the save method!!!
 // Only save if return == false!!!
-        static function checkName($name)
-        {
-            $query = $GLOBALS['DB']->query("SELECT name FROM customers;");
-            $all_names = $query->fetchAll(PDO::FETCH_ASSOC);
-            $exist = false;
+        // static function checkName($name)
+        // {
+        //     $query = $GLOBALS['DB']->query("SELECT name FROM customers;");
+        //     $all_names = $query->fetchAll(PDO::FETCH_ASSOC);
+        //     $exist = false;
+        //
+        //     foreach($all_names as $username){
+        //         if($username['name'] == $name){
+        //             $exist = true;
+        //         }
+        //     }
+        //     return $exist;
+        // }
 
-            foreach($all_names as $username){
-                if($username['name'] == $name){
-                    $exist = true;
-                }
+        function CheckLoginInDB($username,$password){
+            if(!$this->DBLogin()){
+                $this->HandleError("Database login failed!");
+                return false;
+            }         
+            $username = $this->SanitizeForSQL($username);
+            $pwdmd5 = md5($password);
+            $qry = "Select name, email from $this->tablename ".
+                " where username='$username' and password='$pwdmd5' ".
+                " and confirmcode='y'";
+
+            $result = mysql_query($qry,$this->connection);
+
+            if(!$result || mysql_num_rows($result) <= 0){
+                $this->HandleError("Error logging in. ".
+                    "The username or password does not match");
+                return false;
             }
-            return $exist;
+            return true;
+            }
+
+        function Login(){
+            if(empty($_POST['username'])){
+                $this->HandleError("UserName is empty!");
+                return false;
+            }
+            if(empty($_POST['password'])){
+                $this->HandleError("Password is empty!");
+                return false;
+            }
+            $username = trim($_POST['username']);
+            $password = trim($_POST['password']);
+
+            if(!$this->CheckLoginInDB($username,$password)){
+                return false;
+            }
+
+            session_start();
+            $_SESSION[$this->GetLoginSessionVar()] = $username;
+            return true;
         }
+
+
+
+
+
+
 
         function save()
         {
